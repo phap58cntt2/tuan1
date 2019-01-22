75b1b6b36fd51238a91f82d69125544977d72be7
phap58cntt2 <ngocwtf@gmail.com>
2019-01-22 11:37:46 +0700

    Lần 3

---
+++ bai4.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#define MAXINT 1000000
+#define max 100
+#define FileIn "dothi.txt"
+using namespace std;
+void Doc_File(int A[max][max], int &n, int &D, int &C) {
+ FILE*f = fopen(FileIn,"rb");
+ fscanf(f,"%d%d%d",&n,&D,&C);
+ cout<<"Ma Tran Lien Ket Tuong Ung.\n";
+ cout<<D<<" "<<C<<endl;
+ for(int i =0;i<n;i++) {
+  for(int j =0;j<n;j++) {
+   fscanf(f,"%d",&A[i][j]);
+   cout<<A[i][j]<<" ";
+  }
+  cout<<endl; 
+ }
+ fclose(f);
+ D--; C--;
+}
+void Dijkstra(int A[max][max], int n, int D, int C) {
+ char DanhDau[max];
+ int Nhan[max], Truoc[max], XP, min;
+ for(int i=0; i<n; i++){
+  Nhan[i] = MAXINT;
+  DanhDau[i] = 0;
+  Truoc[i] = D;
+ }
+ Nhan[D] = 0;
+ DanhDau[D] = 1;
+ XP = D;
+ while(XP != C){
+  for(int j=0; j<n; j++)
+   if(A[XP][j]>0 && Nhan[j]>A[XP][j]+Nhan[XP] && DanhDau[j]==0) {
+    Nhan[j] = A[XP][j]+Nhan[XP];
+    Truoc[j] = XP;
+   }
+   min = MAXINT;
+  for(int j = 0; j<n; j++)
+  if(min>Nhan[j]&& DanhDau[j]==0){
+   min = Nhan[j];
+   XP = j;
+  }
+  DanhDau[XP] = 1;
+ }
+ cout<<"Duong Di Ngan Nhat La:"<<Nhan[C]<<endl;
+ cout<<C+1<<" <-"<<Truoc[C]+1;
+ int i = Truoc[C];
+ while(i!=D){
+  i = Truoc[i];
+  cout<<" <-"<<i+1;
+ }
+}
+int  main() {
+ int A[max][max],n,Dau,Cuoi;
+
+
+
+ Doc_File(A,n,Dau,Cuoi);
+ Dijkstra(A,n,Dau,Cuoi);
+ getch();
+
+} 
